6f283e1f1b536372f2ac221fee4217d2ce05943c
ankita <ankitapaul3324@gmail.com>
2026-09-02 10:57:09 +0530

    business logic for product service

--- src/main/java/com/techie/microservices/product/service/ProductService.java
+++ src/main/java/com/techie/microservices/product/service/ProductService.java
@@ -4,21 +4,26 @@ import com.techie.microservices.product.dto.ProductRequest;
 import com.techie.microservices.product.model.Product;
 import com.techie.microservices.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class ProductService {
     private final ProductRepository productRepository;
     //add the code to create the product
 
-    public void createProduct(ProductRequest productRequest) {
+    public Product createProduct(ProductRequest productRequest) {
         Product product = Product.builder()
                 .name(productRequest.name())
                 .description(productRequest.description())
                 .price(productRequest.price())
                 .build();
         productRepository.save(product);
+        log.info("Product created successfully");
+        return product;
 
     }
 }
